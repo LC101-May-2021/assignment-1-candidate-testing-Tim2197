358e7492339c29f14aec26dc41424fa588325f41
tim2197 <>
2021-05-26 22:55:45 +0000

    added multiple questions and a grading system

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,11 +8,11 @@ let candidateName = String();
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 
 let question = ("Who was the first American woman in space?: ");
-let correctAnswer;
+let correctAnswer = "Sally Ride";
 let candidateAnswer;
-let questions;
-let correctAnswers = "Sally Ride";
-let candidateAnswers;
+let questions = ["1. Who was the first American woman in space?: ","2. True or false: 5 kilometer == 5000 meters?: ","3. (5 + 3)/2 * 10 = ?: ","4. Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?: ","5. What is the minimum crew size for the ISS?: "]
+let correctAnswers = ["Sally Ride","true","40","Trajectory","3"]
+let candidateAnswers= [];
 
 function askForName (){ 
   candidateName = input.question("What is your name?: ");
@@ -23,21 +23,43 @@ function askForName (){
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question (question);
+//candidateAnswer = input.question (question);
+for (let i = 0; i < questions.length; i++){
+  let answer = input.question(questions[i])
+  candidateAnswers.push(answer);
+  console.log(`Your answer: ${candidateAnswers[i]}\nCorrect answer: ${correctAnswers[i]}\n`)
+  }
+}
 
-if (candidateAnswer === "Sally Ride") {
+
+
+
+/*if (candidateAnswer === "Sally Ride") {
  console.log (`Your answer was "${candidateAnswer}". Your answer is correct.`);
 } else {
   console.log (`Your answer was "${candidateAnswer}". Your answer is incorrect.`);
 }
-}
+}*/
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+  /*console.log (`Your answers were:  ${candidateAnswers[0]}, ${candidateAnswers[1]}, ${candidateAnswers[2]}, ${candidateAnswers[3]}, ${candidateAnswers[4]}. \nThe correct answers were: ${correctAnswers[0]}, ${correctAnswers[1]}, ${correctAnswers[2]}, ${correctAnswers[3]}, ${correctAnswers[4]}.`)*/
   let grade;
+  let correct = 0;
+  for (let j = 0; j < candidateAnswers.length; j++){
+    if (candidateAnswers[j].toUpperCase() === correctAnswers[j].toUpperCase()){
+      correct = correct + 1
+    }
+  }
+  grade = correct / questions.length * 100
+  console.log(`>>> Overall Grade: ${grade}% (${correct} of ${questions.length} responses correct) <<<`)
 
+  if (grade >= 80) {
+    console.log(">>> Status: PASSED <<<")
+  } else {
+    console.log(">>> Status: FAILED <<<")
+  }
 
   return grade;
 }
@@ -46,9 +68,10 @@ function runProgram() {
   askForName();
 
   // TODO 1.1c: Ask for candidate's name //
-  console.log (`Hello, ${candidateName}.`);
+  console.log (`Candidate Name: ${candidateName}.`);
 
   askQuestion();
+
   gradeQuiz(this.candidateAnswers);
 }
 
